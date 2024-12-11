26d03fae3b4f308feb7d30f2d752cea5671f0d8a
Phong <phongchau346@gmail.com>
2024-12-11 10:16:29 +0700

    Create-Routes

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+
+import { ProductDetailPage } from "../pages/ProductDetailPage/ProductDetailPage";
+
+export const routes = [
+  
+  {
+    path: "/product-detail/:id",
+    page: ProductDetailPage,
+    isShowHeader: true,
+    isShowFooter: false,
+  },
+]
