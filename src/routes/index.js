cea6d4fdf30f179fd3429faf9e4b8531dddf26f8
Phong <phongchau346@gmail.com>
2024-12-11 10:57:16 +0700

    Create-Routes

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+
+import { SignUpPage } from "../pages/SignUpPage/SignUpPage";
+
+export const routes = [
+    
+    {
+        path: '/sign-up',
+        page: SignUpPage,
+        isShowHeader: false
+    },
+]
